4cd98fd4fe11dbfa5591055d8c2895be7a07aa96
abhijitdas44719 <abhijitdas44719@gmail.com>
2025-08-27 22:13:39 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
+      '/api': 'https://adbbo-backend.onrender.com'
     },
   },
   plugins: [react()],
